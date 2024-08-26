3221854096273da467fe2eee63e4094c0be45c41
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-08-26 22:46:44 +0700

    init

---
+++ server/src/app.ts
@@ -0,0 +1,40 @@
+import compression from 'compression';
+import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+class App {
+    public app: express.Application;
+    public env: string;
+    public port: string | number;
+    
+    constructor() {
+        this.app = express();
+        this.env = process.env.NODE_ENV || 'dev';
+        this.port = process.env.PORT || 3000;
+
+        this.middlewares();
+    }
+
+    public listen() {
+        this.app.listen(this.port || 3000, () => {
+            console.log(`Server running on port ${this.port}`);
+        })
+    }
+
+    public getServer() {
+        return this.app;
+    }
+
+    private middlewares() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+        this.app.use(helmet());
+        this.app.use(compression());
+        this.app.use(morgan('dev'));
+    }
+}
+
+export default App;
